582aaf08b5dc668574d5a8281ad7c6ec16bf26f7
BushAnalytics <jmnarthur@gmail.com>
2025-04-10 20:23:58 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        beige: '#f5f5dc',
+        bluefire: '#005f73',
+      }
+    },
+  },
+  darkMode: 'class',  // Essential for your dark mode toggle
+  plugins: [],
+}
